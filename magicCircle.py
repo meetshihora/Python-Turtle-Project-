1f6c1e3164e69bbc0079eecd3a1f43f50e8cc03b
Meet Shihora <142139362+meetshihora@users.noreply.github.com>
2024-08-30 11:47:06 +0530

    Add files via upload

---
+++ magicCircle.py
@@ -0,0 +1,16 @@
+import turtle
+a= turtle.Turtle()
+b = turtle.Screen()
+b.bgcolor("white")
+a.pencolor("red")
+a.speed(50)
+for i in range(250):
+    a.circle(100)
+    a.lt(1)
+    a.circle(100)
+    a.lt(1)
+
+
+
+
+
